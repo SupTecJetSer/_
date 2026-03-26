607cd6ab939e9534499b7a710d24346763cd0294
SupTecJetSer <suporte.tecnico@jetservice.net.br>
2026-03-26 13:35:55 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -32,8 +32,8 @@ function copiarCodigo(codigo, el) {
     icon.innerHTML = "✔️";
 
     el._copyTimeout = setTimeout(() => {
-      icon.innerHTML = "📋";
-    }, 1000);
+      icon.innerHTML = "";
+    }, 1500);
   }
 
   if (linha) {
@@ -139,7 +139,7 @@ function renderizar() {
     tabela.innerHTML += `
       <tr class="${item.tipo}">
         <td class="codigo-cell" onclick="copiarCodigo('${item.codigo}', this)">
-          <span class="copy-btn">📋</span>
+          <span class="copy-btn"></span>
           ${item.codigo}
         </td>
         <td>
